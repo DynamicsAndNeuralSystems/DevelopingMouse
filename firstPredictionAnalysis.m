40bfa27c45054373ec605071c0dfdcada0eea168
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2017-06-05 20:55:02 +1000

    Commented out analysis of degree Perform GO enrichment analysis Save SVM prediction accuracies and GO enrichment analysis results

--- firstPredictionAnalysis.m
+++ firstPredictionAnalysis.m
@@ -5,14 +5,20 @@ load('newmatrixData.mat','h','V','k','geneEntrez');
 [~,ix] = sort(h,'descend');
 % Create empty cell array to store gene info strings 
 diffExpSeqHub=cell(7,1);
-diffExpSeqDeg=cell(7,1);
+% diffExpSeqDeg=cell(7,1);
 geneEntrezleft=cell(7,1); 
-% Order by degree
-[~,ixk] = sort(k,'descend')
-% Define high-degree nodes (i.e. nodes with a degree at least one standard deviation above the network mean)
-z=zscore(k);
-% Create a vector of indices of high-degree nodes
-highDegNode=z>=1;
+% Create empty cell to store GOtables
+GOTableCell=cell(7,1);
+% Create empty cell to store Gene Entrez Annotations of the GO tables
+geneEntrezAnnotationsCell=cell(7,1);
+% Create empty cell to store accuracies (mean,1 SD) of SVM prediction 
+accuraciesSvmCell=cell(7,1);
+% % Order by degree
+% [~,ixk] = sort(k,'descend')
+% % Define high-degree nodes (i.e. nodes with a degree at least one standard deviation above the network mean)
+% z=zscore(k);
+% % Create a vector of indices of high-degree nodes
+% highDegNode=z>=1;
 
 %% Loop the analysis through all developmental stages
 for i=1:7
@@ -38,9 +44,11 @@ for i=1:7
     % Compute differences:
     numGenes = size(V{i},2);
     tStats = zeros(numGenes,1);
+    pValHub = zeros(numGenes,1); %Create zero vector to store p values
     for j = 1:numGenes
-        [~,~,~,stats] = ttest2(V{i}(h==0,j),V{i}(h==1,j));
+        [~,p,~,stats] = ttest2(V{i}(h==0,j),V{i}(h==1,j));
         tStats(j) = stats.tstat;
+        pValHub(j)=p;
     end
     % Sort by differences:
     [~,iy] = sort(tStats,'descend');
@@ -55,35 +63,19 @@ for i=1:7
     imagesc(vNorm{i}(ix,iy))
     title('Normalized gene expression sorted by hub status in rows and differential expression in columns')
     
-    % Analysis with degree
-    % Plot normalized, sorted by degree:
-    f = figure('color','w');
-    imagesc(vNorm{i}(ixk,:))
-    title('Normalized gene expression sorted by degree in rows')
-
-    % Compute differences between high and low degree nodes:
-    numGenes = size(V{i},2);
-    tStats = zeros(numGenes,1);
-        for k = 1:numGenes
-            [~,~,~,stats] = ttest2(V{i}(z==0,i),V{i}(z==1,i));
-            tStats(k) = stats.tstat;
-        end
-
-        % Sort by differences:
-        [~,iyk] = sort(tStats,'descend');
-
-        % Print the gene entrez of genes with differential expression between high degree vs low degree nodes, from highest to lowest
-        %diffExpSeqDeg{i}=geneEntrezleft{i}(iyk);
-        %fprintf('Genes with differential expression between high degree vs low degree nodes in descending order of difference: \n')
-        %diffExpSeqDeg{i}
-
-    % Plot normalized, sorted by degree and t-statistic:
-    f = figure('color','w');
-    imagesc(vNorm{i}(ixk,iyk))
-    title('Normalized gene expression sorted by degree in rows and differential expression in columns')
-
-
-    %-------------------------------------------------------------------------------
+    % Plot p value against gene
+    %gene=1:numGenes;
+    %plot(gene,pValHub)
+    %x_labels=geneEntrezleft{i};
+    %xlabel('Gene entrez ID')
+    %ylabel('p value')
+    %set(gca,'xtick',gene,'xticklabel',x_labels)
+    
+    %gene enrichment analysis
+    [GOTable,geneEntrezAnnotations]=SingleEnrichment(tStats,geneEntrezleft{i})
+    GOTableCell{i}=GOTable;
+    geneEntrezAnnotationsCell{i}=geneEntrezAnnotations;
+      %-------------------------------------------------------------------------------
     %% Machine learning prediction of hub/nonhub:
     %-------------------------------------------------------------------------------
     hLabels = h+1; % hubs are '2', nonhubs are '1'
@@ -94,17 +86,53 @@ for i=1:7
                                 2,true,'balancedAcc',true,5);
             accuracies(m) = mean(accuracy);
         end
+    
     fprintf(1,'Balanced classification accuracy = %.1f +/- %.1f%%\n',mean(accuracies),std(accuracies));
-    %-------------------------------------------------------------------------------
-    %% Machine learning prediction of high vs low degree nodes:
-    %-------------------------------------------------------------------------------
-    zLabel = highDegNode+1; % high degree nodes are '2', low degree nodes are '1'
-    numRepeats = 200;
-    accuracies = zeros(numRepeats,1);
-        for n = 1:numRepeats
-            [accuracy,Mdl,whatLoss] = GiveMeCfn('svm_linear',vNorm{i},zLabel,vNorm{i},zLabel,...
-                                2,true,'balancedAcc',true,3);
-            accuracies(n) = mean(accuracy);
-        end
-    fprintf(1,'Balanced classification accuracy = %.1f +/- %.1f%%\n',mean(accuracies),std(accuracies));
+    accuraciesSvmCell{i}=[mean(accuracies),std(accuracies)];
 end
+save('GOresults.mat','GOTableCell','geneEntrezAnnotationsCell')
+save('SVMaccuracies.mat','accuraciesSvmCell')
+
+% %%
+%     % Analysis with degree
+%     % Plot normalized, sorted by degree:
+%     f = figure('color','w');
+%     imagesc(vNorm{i}(ixk,:))
+%     title('Normalized gene expression sorted by degree in rows')
+% 
+%     % Compute differences between high and low degree nodes:
+%     numGenes = size(V{i},2);
+%     tStats = zeros(numGenes,1);
+%         for k = 1:numGenes
+%             [~,~,~,stats] = ttest2(V{i}(highDegNode==0,k),V{i}(highDegNode==1,k));
+%             tStats(k) = stats.tstat;
+%         end
+%         
+%         % Sort by differences:
+%         [~,iyk] = sort(tStats,'descend');
+% 
+%         % Print the gene entrez of genes with differential expression between high degree vs low degree nodes, from highest to lowest
+%         %diffExpSeqDeg{i}=geneEntrezleft{i}(iyk);
+%         %fprintf('Genes with differential expression between high degree vs low degree nodes in descending order of difference: \n')
+%         %diffExpSeqDeg{i}
+% 
+%     % Plot normalized, sorted by degree and t-statistic:
+%     f = figure('color','w');
+%     imagesc(vNorm{i}(ixk,iyk))
+%     title('Normalized gene expression sorted by degree in rows and differential expression in columns')
+% 
+% 
+%     
+%     %-------------------------------------------------------------------------------
+%     %% Machine learning prediction of high vs low degree nodes:
+%     %-------------------------------------------------------------------------------
+%     zLabel = highDegNode+1; % high degree nodes are '2', low degree nodes are '1'
+%     numRepeats = 200;
+%     accuracies = zeros(numRepeats,1);
+%         for n = 1:numRepeats
+%             [accuracy,Mdl,whatLoss] = GiveMeCfn('svm_linear',vNorm{i},zLabel,vNorm{i},zLabel,...
+%                                 2,true,'balancedAcc',true,3);
+%             accuracies(n) = mean(accuracy);
+%         end
+%     fprintf(1,'Balanced classification accuracy = %.1f +/- %.1f%%\n',mean(accuracies),std(accuracies));
+    
