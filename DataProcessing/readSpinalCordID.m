234402de4229e8cc45c7ff648b1812368ca20cdb
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-04-16 14:33:05 +1000

    Modified file path of readSpinalCordID

--- DataProcessing/readSpinalCordID.m
+++ DataProcessing/readSpinalCordID.m
@@ -1,5 +1,5 @@
 % user input: location to save the created matlab variable
-save_loc='D:\Data\DevelopingMousePhase2\readSpinalCordID';
+save_loc=fullfile('Data','Matlab_variables');
 % import spinal cord IDs
 spinalCord_ID=csvread('SpC_descendantID.csv',1,1,[1 1 93 1]);
 str=strcat(save_loc,'\','spinalCord_ID.mat');
