5e3808c5ed1d18f3577bc06ba24bf2fbdf3499ec
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-04-16 11:05:29 +1000

    Added script to download unionize from API

--- startup.m
+++ startup.m
@@ -1,2 +1,9 @@
-addpath(genpath(fullfile(fileparts(mfilename('fullpath')))));
-
+% Add paths required for the project (ignoring hidden, including version control)
+cd(fileparts(which(mfilename)))
+files = dir;
+directories = files([files.isdir]);
+directories(strmatch('.',{files([files.isdir]).name})) = []; % remove hidden
+paths = arrayfun(@(x)fullfile(directories(x).name),1:length(directories),'UniformOutput',false);
+for j = 1:length(paths)
+    addpath(genpath(paths{j}))
+end
