c189dec11103db0b0435aae792b797f18ccddc3e
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-04-24 16:52:58 +1000

    Updated plotGeneCoexpression_scatter_voxel

--- Analysis/plotGeneCoexpression_scatter_voxel.m
+++ Analysis/plotGeneCoexpression_scatter_voxel.m
@@ -10,6 +10,7 @@ function [f,F,distances_all,corrCoeff_all]=plotGeneCoexpression_scatter_voxel(..
   % plot coexpression against distance
   f=figure('color','w','Position',get(0, 'Screensize'));
   gcf;
+  str = sprintf('Developing Mouse %s %s',timePointNow,brainDiv);
   if densityOn==1
     % xBin_num=0.1*(max(distances_all)-min(distances_all));
     % yBin_num=xBin_num;
@@ -28,6 +29,7 @@ function [f,F,distances_all,corrCoeff_all]=plotGeneCoexpression_scatter_voxel(..
     p=plot(xData,fitting_stat_all.voxel.(timePointNow).fHandle.exp(xData));
     xlabel('Separation Distance (um)','FontSize',16)
     ylabel('Gene Coexpression (Pearson correlation coefficient)','FontSize',13)
+    title(str,'Fontsize',19);
     % Plot heatmap:
     subplot(2, 1, 2);
     imagesc(Xedges, Yedges, N);
@@ -35,6 +37,7 @@ function [f,F,distances_all,corrCoeff_all]=plotGeneCoexpression_scatter_voxel(..
     set(gca, 'XLim', Xedges([1 end]), 'YLim', Yedges([1 end]), 'YDir','normal');
     xlabel('Separation Distance (um)','FontSize',16)
     ylabel('Gene Coexpression (Pearson correlation coefficient)','FontSize',13)
+    title(str,'Fontsize',19);
   elseif densityOn==0
     scatter(distances_all,corrCoeff_all,'.')
     % add exponential fitting
@@ -42,10 +45,9 @@ function [f,F,distances_all,corrCoeff_all]=plotGeneCoexpression_scatter_voxel(..
     p=plot(xData,fitting_stat_all.voxel.(timePointNow).fHandle.exp(xData));
     xlabel('Separation Distance (um)','FontSize',16)
     ylabel('Gene Coexpression (Pearson correlation coefficient)','FontSize',13)
+    title(str,'Fontsize',19);
   end
   set(p,'Color','k','LineWidth',5)
   legend(p,'Exponential fit')
-  str = sprintf('Developing Mouse %s %s',timePointNow,brainDiv);
-  title(str,'Fontsize',19);
   F=getframe(f);
 end
