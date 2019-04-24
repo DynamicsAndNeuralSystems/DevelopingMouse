9f782207e593f67ce4f501786a042120ea77dbc6
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-04-24 14:53:06 +1000

    Updated plotGeneCoexpression_scatter_voxel

--- Analysis/plotGeneCoexpression_scatter_voxel.m
+++ Analysis/plotGeneCoexpression_scatter_voxel.m
@@ -19,25 +19,28 @@ function [f,F,distances_all,corrCoeff_all]=plotGeneCoexpression_scatter_voxel(..
     xBin=linspace(0,max(distances_all),xBin_num);
     yBin=linspace(-0.4,1,yBin_num);
     % Bin the data:
-    [N,~,~]=histcounts2(corrCoeff_all(:), distances_all(:), xBin, yBin);
+    N=histcounts2(distances_all(:), corrCoeff_all(:), xBin, yBin);
     % Plot scattered data (for comparison):
     subplot(2, 1, 1);
     scatter(distances_all,corrCoeff_all,'.');
     set(gca, 'XLim', xBin([1 end]), 'YLim', yBin([1 end]));
     hold on
     % add exponential fitting
-    xData=linspace(min(distances_all),max(corrCoeff_all),0.1*length(distances_all));
-    plot(xData,fitting_stat_all.voxel.(timePointCell{1}).fHandle.exp(xData));
+    xData=linspace(min(distances_all),max(distances_all),0.1*length(distances_all));
+    p=plot(xData,fitting_stat_all.voxel.(timePointCell{1}).fHandle.exp(xData));
     % Plot heatmap:
     subplot(2, 1, 2);
     imagesc(xBin, yBin, N);
     set(gca, 'XLim', xBin([1 end]), 'YLim', yBin([1 end]));
   elseif densityOn==0
     scatter(distances_all,corrCoeff_all,'.')
+    set(gca, 'XLim', xBin([1 end]), 'YLim', yBin([1 end]));
     % add exponential fitting
-    xData=linspace(min(distances_all),max(corrCoeff_all),0.1*length(distances_all));
-    plot(xData,fitting_stat_all.voxel.(timePointCell{1}).fHandle.exp(xData));
+    xData=linspace(min(distances_all),max(distances_all),0.1*length(distances_all));
+    p=plot(xData,fitting_stat_all.voxel.(timePointCell{1}).fHandle.exp(xData));
   end
+  set(p,'Color','k')
+  legend(p,'Exponential fit')
   xlabel('Separation Distance (um)','FontSize',16)
   ylabel('Gene Coexpression (Pearson correlation coefficient)','FontSize',13)
   str = sprintf('Developing Mouse %s %s',timePointNow,brainDiv);
