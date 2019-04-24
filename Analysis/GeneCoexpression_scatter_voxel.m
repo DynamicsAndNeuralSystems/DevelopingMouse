25239319f49e9d0b4e3fa88db8343a996a1808f0
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-04-24 13:38:09 +1000

    Corrected variable loading in GeneCoexpression_scatter_voxel

--- Analysis/GeneCoexpression_scatter_voxel.m
+++ Analysis/GeneCoexpression_scatter_voxel.m
@@ -2,7 +2,7 @@
 % initialize and load variables
 %---------------------------------------------------------------------
 timePoints={'E11pt5','E13pt5','E15pt5','E18pt5','P4','P14','P28'};
-load('voxelGeneCoexpression_all') % contains 'voxGeneMat_all','distMat_all','dataIndSelect_all'
+load('voxelGeneCoexpression_all.mat') % contains 'voxGeneMat_all','distMat_all','dataIndSelect_all'
 %
 % initialize
 distances_all = cell(length(timePoints),1);
