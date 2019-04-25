597583e2525662b0575c20c6723b8109f046daf3
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-04-26 01:08:51 +1000

    Edited plotGeneCoexpression_scatter_voxel changing BF_normalizeMatrix back to maxmin

--- Analysis/createBinnedData.m
+++ Analysis/createBinnedData.m
@@ -1,11 +1,16 @@
 clear
 load('fitting.mat','spatialData');
 numThresholds=100;
+timePoints={'E11pt5','E13pt5','E15pt5','E18pt5','P4','P14','P28'};
+xPlotDataAll_scaled=cell(length(timePoints),1);
 % Bin
 [~,~,xPlotDataAll,yPlotDataAll] = plotBinning(spatialData.voxel.distancesAll,...
                                         spatialData.voxel.corrCoeffAll,...
                                         numThresholds);
+% Bin and then scale the distance
+for i=1:length(timePoints)
+  xPlotDataAll_scaled{i}=xPlotDataAll{i}/max(xPlotDataAll{i});
+end
 % save variable
 str=fullfile('Matlab_variables',strcat('binnedData','_','numThresholds',num2str(numThresholds),'.mat'));
-save(str,'xPlotDataAll','yPlotDataAll','numThresholds')
-% Bin and then scale the distance
+save(str,'xPlotDataAll','yPlotDataAll','xPlotDataAll_scaled','numThresholds')
