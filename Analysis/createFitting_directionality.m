d0cc48608ac9ac9d924688d467110f2c6115b438
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-04-28 22:57:42 +1000

    Troubleshooting createFitting_directionality

--- Analysis/createFitting_directionality.m
+++ Analysis/createFitting_directionality.m
@@ -1,4 +1,5 @@
 timePoints={'E11pt5','E13pt5','E15pt5','E18pt5','P4','P14','P28'};
+load('directionalityIndex.mat')
 filestr='spatialData_NumData_1000';
 load(strcat(filestr,'.mat'));
 file_parts=strsplit(filestr,'_');
@@ -9,30 +10,42 @@ distances_all_scaled=cell(length(timePoints),1);
 distances_direction=struct();
 corrCoeff_direction=struct();
 distances_direction_scaled=struct();
+distances_sagittal=cell(length(timePoints),1);
+corrCoeff_sagittal=cell(length(timePoints),1);
+
 for i=1:length(timePoints)
   distances_all_scaled{i}=distances_all{i}/max(distances_all{i});
 end
-
-for j=1:length(directions)
-  distances_direction.(directions{j})=cell(length(timePoints),1);
-  corrCoeff_direction.(directions{j})=cell(length(timePoints),1);
-  distances_direction_scaled.(directions{j})=cell(length(timePoints),1);
-  for i=1:length(timePoints) % for each time point extract the pairs in the direction
-    distances_direction.(directions{j}){i}=distances_all{i}(strcat('is',directions{j}));
-    corrCoeff_direction.(directions{j}){i}=corrCoeff_all{i}(strcat('is',directions{j}));
-    distances_direction_scaled.(directions{j}){i}=distances_all_scaled{i}(strcat('is',directions{j}));
-  end
+% extract distances and correlation coefficients
+for i=1:length(timePoints)
+  distances_sagittal{i}=distances_all{i}(isSagittal{i});
+  corrCoeff_sagittal{i}=corrCoeff_all{i}(isSagittal{i});
 end
-
-% create fitting
-for j=1:length(directions) % for each direction
-  % not scaled
-  [fitting_stat_all, decayConstant, maxDistance]=getFitting(distances_direction.(directions{j}),...
-                                                            corrCoeff_direction.(directions{j}));
-  str=fullfile('Matlab_variables',strcat('fitting_NumData_',NumData,'_',directions{j},'.mat'));
+% try sagittal only (troubleshooting)
+[fitting_stat_all, decayConstant, maxDistance]=getFitting(distances_sagittal,corrCoeff_sagittal);
+  str=fullfile('Matlab_variables',strcat('fitting_NumData_',NumData,'_sagittal','.mat'));
   save(str,'fitting_stat_all','decayConstant','maxDistance');
-  % scaled distance
-  [fitting_stat_all, decayConstant, ~]=getFitting(distances_direction_scaled.(directions{j}),corrCoeff_direction.(directions{j}));
-  str=fullfile('Matlab_variables',strcat('fitting_NumData_',NumData,'_',directions{j},'_scaled','.mat'));
-  save(str,'fitting_stat_all','decayConstant','maxDistance'); % still save the original max distance
-end
+
+% for j=1:length(directions)
+%   distances_direction.(directions{j})=cell(length(timePoints),1);
+%   corrCoeff_direction.(directions{j})=cell(length(timePoints),1);
+%   distances_direction_scaled.(directions{j})=cell(length(timePoints),1);
+%   for i=1:length(timePoints) % for each time point extract the pairs in the direction
+%     distances_direction.(directions{j}){i}=distances_all{i}(strcat('is',directions{j}));
+%     corrCoeff_direction.(directions{j}){i}=corrCoeff_all{i}(strcat('is',directions{j}));
+%     distances_direction_scaled.(directions{j}){i}=distances_all_scaled{i}(strcat('is',directions{j}));
+%   end
+% end
+%
+% % create fitting
+% for j=1:length(directions) % for each direction
+%   % not scaled
+%   [fitting_stat_all, decayConstant, maxDistance]=getFitting(distances_direction.(directions{j}),...
+%                                                             corrCoeff_direction.(directions{j}));
+%   str=fullfile('Matlab_variables',strcat('fitting_NumData_',NumData,'_',directions{j},'.mat'));
+%   save(str,'fitting_stat_all','decayConstant','maxDistance');
+%   % scaled distance
+%   [fitting_stat_all, decayConstant, ~]=getFitting(distances_direction_scaled.(directions{j}),corrCoeff_direction.(directions{j}));
+%   str=fullfile('Matlab_variables',strcat('fitting_NumData_',NumData,'_',directions{j},'_scaled','.mat'));
+%   save(str,'fitting_stat_all','decayConstant','maxDistance'); % still save the original max distance
+% end
