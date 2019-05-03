6909ddb665710d34067b1665835871a613f7dc25
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-05-03 18:01:43 +1000

    Updated createSpatialData_brainDivision_goodGeneSubset

--- DataProcessing/createSpatialData_brainDivision_goodGeneSubset.m
+++ DataProcessing/createSpatialData_brainDivision_goodGeneSubset.m
@@ -1,4 +1,4 @@
 % create correlation coefficient and distances
-[~,~,~,~,~]=makeSpatialData(1000,true,'forebrain');
-[~,~,~,~,~]=makeSpatialData(1000,true,'midbrain');
-[~,~,~,~,~]=makeSpatialData(1000,true,'hindbrain');
+[~,~,~,~,~]=makeSpatialData(1000,true,'forebrain',false);
+[~,~,~,~,~]=makeSpatialData(1000,true,'midbrain',false);
+[~,~,~,~,~]=makeSpatialData(1000,true,'hindbrain',false);
