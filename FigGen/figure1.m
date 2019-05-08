ccb98a080ed52633c9ccd37c465a9015d790f122
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-05-08 12:38:22 +1000

    Working on figure 1

--- FigGen/figure1.m
+++ FigGen/figure1.m
@@ -9,7 +9,7 @@ timePoints = GiveMeParameter('timePoints');
 numSubplot1=7;
 numSubplotAll=10;
 f=figure('color','w','Position',get(0,'Screensize'));
-for j=1%:numSubplot1
+for j=1:numSubplot1
   subplot(2,4,j)
   makeBinningPlot_withExponential(numData,numThresholds,...
                                   thisBrainDiv,scaledDistance,...
