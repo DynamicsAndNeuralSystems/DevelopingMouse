1094aeeb5d4fa9da630df2a94292ad07500bfd7c
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2019-05-28 01:15:27 +0800

    Randomize sequence of direction vector generation in makeDirectionality

--- Analysis/makeDirectionality.m
+++ Analysis/makeDirectionality.m
@@ -1,16 +1,10 @@
-function [angle_coronal,angle_axial,angle_sagittal]=makeDirectionality(coOrds)
-  % function [angle_coronal,angle_axial,angle_sagittal]=makeDirectionality(coOrds)
-  % determine directionality
-  % read in the coordinates and voxgenemat
+function [angle_coronal,angle_axial,angle_sagittal]=makeDirectionality(coOrds,shuffledOrder)
+  % this function determines directionality of voxel pairs; shuffledOrder is a shuffled sequence
+  % for reordering the sequence of computing the direction vectors
+  % the three vectors in the main direciton
   coronalVec=[1 0 0]; % front-back
   axialVec=[0 1 0]; % top-down
   sagittalVec=[0 0 1]; % left-right
-
-  % determine angle made with the three directions between two voxels
-  % vec=[1 2 3] % for example
-  % angle_coronal=acos((dot(vec,coronalVec))/(norm(vec)*norm(coronalVec)));
-  % angle_axial=acos((dot(vec,axialVec))/(norm(vec)*norm(axialVec)));
-  % angle_sagittal=acos((dot(vec,sagittalVec))/(norm(vec)*norm(sagittalVec)));
 
   [n,p] = size(coOrds);
   angle_sagittal = zeros(1,n*(n-1)./2);
@@ -20,29 +14,12 @@ function [angle_coronal,angle_axial,angle_sagittal]=makeDirectionality(coOrds)
   % make the direction vectors
   vecMat = zeros(n*(n-1)./2,3); % stores direction vectors
   C = combnk(1:n,2); % obtain the combination indexes of two coOrds taken at a time
-  for i = 1:length(vecMat)
-    % for j = 1:n-1
-    %   for k = j+1:n
+  C = C(shuffledOrder,:); % shuffle order to eliminate left-right bias inherent in coOrds
+  for i = 1:size(vecMat,1)
     vecMat(i,:) = coOrds(C(i,2),:)-coOrds(C(i,1),:);
     % determine angle made with the three directions
     angle_coronal(i) = acos((dot(vecMat(i,:),coronalVec))/(norm(vecMat(i,:))*norm(coronalVec)));
     angle_axial(i) = acos((dot(vecMat(i,:),axialVec))/(norm(vecMat(i,:))*norm(axialVec)));
     angle_sagittal(i) = acos((dot(vecMat(i,:),sagittalVec))/(norm(vecMat(i,:))*norm(sagittalVec)));
-    %   end
-    % end
   end
-  %%
-  % k = 1;
-  % for i = 1:n-1 % for each coordinate triplet
-  %     % determine direction vector with another coordinate triplet
-  %     vecMat = zeros(n-i,p); % pair with another coordinate triplet
-  %     for j = 1:n-1
-  %       vecMat(j,:)=coOrds(j+1,:)-coOrds(i,:); % direction vector
-  %     end
-  %     % determine the angle made with the three directions between two voxels
-  %     angle_coronal(k:(k+n-i-1)) = acos((dot(vecMat(j,:),coronalVec))/(norm(vecMat(j,:))*norm(coronalVec)));
-  %     angle_axial(k:(k+n-i-1)) = acos((dot(vecMat(j,:),axialVec))/(norm(vecMat(j,:))*norm(axialVec)));
-  %     angle_sagittal(k:(k+n-i-1)) = acos((dot(vecMat(j,:),sagittalVec))/(norm(vecMat(j,:))*norm(sagittalVec)));
-  %     k = k + (n-i);
-  % end
 end
--- Analysis/sampleGridData.m
+++ Analysis/sampleGridData.m
@@ -4,23 +4,28 @@ function [distances,corrCoeff,angle_coronal,angle_axial,angle_sagittal]=sampleGr
   resolutionGrid=GiveMeParameter('resolutionGrid');
   timePointIndex=find(strcmp(timePointNow,timePoints));
   [dataIndSelect,~] = datasample([1:size(voxGeneMat,1)],whatNumData,'replace',false);
-  distMat=squareform(pdist(coOrds(dataIndSelect,:),...
+  distMat = squareform(pdist(coOrds(dataIndSelect,:),...
                         'euclidean')*resolutionGrid.(timePoints{timePointIndex}));
   % extract the correlation coefficients
-  geneCorr=corrcoef((voxGeneMat(dataIndSelect,:))','rows','pairwise');
-  corrCoeff=geneCorr(find(triu(ones(size(geneCorr)),1)));
+  geneCorr = corrcoef((voxGeneMat(dataIndSelect,:))','rows','pairwise');
+  corrCoeff = extractDistances(geneCorr);
   % extract distances from distance matrix
   if scaledDistance
-    distances=extractDistances(distMat)/max(extractDistances(distMat));
+    distances = extractDistances(distMat)/max(extractDistances(distMat));
   else
-    distances=extractDistances(distMat);
+    distances = extractDistances(distMat);
   end
   if withDirection
+    % shuffle the distances and corrCoeff
+    shuffledOrder = randperm(whatNumData);
+    distances = distances(shuffledOrder);
+    corrCoeff = corrCoeff(shuffledOrder);
     % determine directionality
-    [angle_coronal,angle_axial,angle_sagittal]=makeDirectionality(coOrds(dataIndSelect,:));
-    angle_coronal=extractDistances(squareform(angle_coronal));
-    angle_axial=extractDistances(squareform(angle_axial));
-    angle_sagittal=extractDistances(squareform(angle_sagittal));
+    [angle_coronal,angle_axial,angle_sagittal]=makeDirectionality(coOrds(dataIndSelect,:),...
+                                                                  shuffledOrder);
+    % angle_coronal=extractDistances(squareform(angle_coronal));
+    % angle_axial=extractDistances(squareform(angle_axial));
+    % angle_sagittal=extractDistances(squareform(angle_sagittal));
   else
     angle_coronal=NaN;
     angle_axial=NaN;
