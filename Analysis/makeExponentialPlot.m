8d5db1ae41a5f02a631b6a60193a38378c01e8c4
Ben Fulcher <ben.d.fulcher@gmail.com>
2019-07-11 16:01:40 +1000

    minimal cleaning

--- Analysis/makeExponentialPlot.m
+++ Analysis/makeExponentialPlot.m
@@ -1,8 +1,9 @@
 function makeExponentialPlot(numData,numThresholds,...
                             thisBrainDiv,scaledDistance,...
                             thisDirection,thisCellType,makeNewFigure)
-% plots all exponential curves from different time points on the same graph
-timePoints=GiveMeParameter('timePoints');
+% Plots all exponential curves from different time points on the same graph
+
+timePoints = GiveMeParameter('timePoints');
 [xPlotDataAll,yPlotDataAll,numThresholds] = makeBinnedData(numData,...
                                                             numThresholds,...
                                                             thisBrainDiv,...
@@ -18,35 +19,35 @@ timePoints=GiveMeParameter('timePoints');
 brainStr = GiveMeFileName(thisBrainDiv);
 cellTypeStr = GiveMeFileName(thisCellType);
 if scaledDistance
-  distanceStr = GiveMeFileName('scaled');
+    distanceStr = GiveMeFileName('scaled');
 else
-  distanceStr = GiveMeFileName('notScaled');
+    distanceStr = GiveMeFileName('notScaled');
 end
 
 if strcmp(thisDirection,'allDirections')
-  fileString = sprintf('spatialData_NumData_%d%s%s%s.mat',numData,brainStr,cellTypeStr,...
+    fileString = sprintf('spatialData_NumData_%d%s%s%s.mat',numData,brainStr,cellTypeStr,...
                     distanceStr);
 else
-  fileString = sprintf('directionalityData_%s%s.mat',thisDirection,distanceStr);
+    fileString = sprintf('directionalityData_%s%s.mat',thisDirection,distanceStr);
 end
 
 load(fileString,'distances_all','corrCoeff_all');
 
-cmapOut = BF_getcmap('dark2',7,0,0);
 if scaledDistance
-  xLabeling=GiveMeLabelName('scaledDistance');
+    xLabeling = GiveMeLabelName('scaledDistance');
 else
-  xLabeling=GiveMeLabelName('originalDistance');
+    xLabeling = GiveMeLabelName('originalDistance');
 end
-yLabeling=GiveMeLabelName('CGE');
+yLabeling = GiveMeLabelName('CGE');
 if makeNewFigure
-  f = figure('color','w');
+    f = figure('color','w');
 end
 for i=1:length(timePoints)
-  plotFitting_singleTimePoint(distances_all,'exp',fitting_stat_all,...
+    plotFitting_singleTimePoint(distances_all,'exp',fitting_stat_all,...
                               xLabeling, yLabeling, 1, ...
                               thisDirection,timePoints{i},false, ...
                               thisBrainDiv,thisCellType);
-  hold on
+    hold('on');
 end
+
 end
