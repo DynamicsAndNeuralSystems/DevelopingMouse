7dc2498d68cfd28efe9254551aa1885ae4e0fc21
Ben Fulcher <ben.d.fulcher@gmail.com>
2019-07-16 16:54:14 +1000

    Editing for clarity

--- FigGen/makeFigure1.m
+++ FigGen/makeFigure1.m
@@ -1,49 +1,57 @@
 function makeFigure1(numData,numThresholds)
-  % plot the voxGeneMat, distMat and cgeMat(correlated gene expression) and the bins
-  timePoints = GiveMeParameter('timePoints');
-  timePointNow = 'E11pt5';
-  thisCellType = 'allCellTypes';
-  thisBrainDiv = 'wholeBrain';
-  brainStr = GiveMeFileName(thisBrainDiv);
-  cellTypeStr = GiveMeFileName(thisCellType);
-  distanceStr = GiveMeFileName('notScaled');
-  matTypes = GiveMeParameter('matTypes'); % {'voxGeneMat','distMat','cgeMat'};
-  timePointIndex = strcmp(timePointNow,timePoints);
-  if nargin < 2
-    numThresholds=GiveMeParameter('numThresholds');
-  end
-  if nargin < 1
-    numData=GiveMeParameter('numData'); 
-  end
+% plot the voxGeneMat, distMat and cgeMat(correlated gene expression) and the bins
 
-  % load distance and correlation data first
-  fileString = sprintf('spatialData_NumData_%d%s%s%s.mat',numData,brainStr,cellTypeStr,...
-                        distanceStr);
-  load(fileString,'distances_all','corrCoeff_all');
-  % plots the voxGeneMat and CGE vs distance graph
-  f1 = figure('color','w');
-  plotMat(timePointNow,thisCellType,thisBrainDiv,matTypes{1},false);
+if nargin < 1
+    numData = GiveMeParameter('numData');
+end
+if nargin < 2
+    numThresholds = GiveMeParameter('numThresholds');
+end
 
-  % plots the distMat and cgeMat
-  f2 = figure('color','w');
-  subplot(2,1,1)
-  plotMat(timePointNow,thisCellType,thisBrainDiv,matTypes{2},false);
-  hold on
-  subplot(2,1,2)
-  plotMat(timePointNow,thisCellType,thisBrainDiv,matTypes{3},false);
+timePoints = GiveMeParameter('timePoints');
+timePointNow = 'E11pt5';
+thisCellType = 'allCellTypes';
+thisBrainDiv = 'wholeBrain';
+brainStr = GiveMeFileName(thisBrainDiv);
+cellTypeStr = GiveMeFileName(thisCellType);
+distanceStr = GiveMeFileName('notScaled');
+matTypes = GiveMeParameter('matTypes'); % {'voxGeneMat','distMat','cgeMat'};
+timePointIndex = strcmp(timePointNow,timePoints);
 
-  f3 = figure('color','w');
-  BF_PlotQuantiles(distances_all{timePointIndex},corrCoeff_all{timePointIndex},...
-                  numThresholds,false,false);
-  xLabel = GiveMeLabelName('originalDistance');
-  yLabel = GiveMeLabelName('CGE');
-  xlabel(xLabel);
-  ylabel(yLabel);
-  % save outs
-  str = fullfile('Outs','figure1','figure1_part1.svg');
-  saveas(f1,str)
-  str = fullfile('Outs','figure1','figure1_part2.svg');
-  saveas(f2,str)
-  str = fullfile('Outs','figure1','figure1_part3.svg');
-  saveas(f3,str)
+%-------------------------------------------------------------------------------
+% Load distance and correlation data:
+fileString = sprintf('spatialData_NumData_%d%s%s%s.mat',numData,brainStr,cellTypeStr,...
+                    distanceStr);
+load(fileString,'distances_all','corrCoeff_all');
+
+%-------------------------------------------------------------------------------
+% Plot the voxGeneMat and CGE vs distance graph:
+f1 = figure('color','w');
+plotMat(timePointNow,thisCellType,thisBrainDiv,matTypes{1},false);
+
+% plots the distMat and cgeMat
+f2 = figure('color','w');
+subplot(2,1,1)
+plotMat(timePointNow,thisCellType,thisBrainDiv,matTypes{2},false);
+hold on
+subplot(2,1,2)
+plotMat(timePointNow,thisCellType,thisBrainDiv,matTypes{3},false);
+
+f3 = figure('color','w');
+BF_PlotQuantiles(distances_all{timePointIndex},corrCoeff_all{timePointIndex},...
+              numThresholds,false,false);
+xLabel = GiveMeLabelName('originalDistance');
+yLabel = GiveMeLabelName('CGE');
+xlabel(xLabel);
+ylabel(yLabel);
+
+%-------------------------------------------------------------------------------
+% Save figures out to svg:
+str = fullfile('Outs','figure1','figure1_part1.svg');
+saveas(f1,str)
+str = fullfile('Outs','figure1','figure1_part2.svg');
+saveas(f2,str)
+str = fullfile('Outs','figure1','figure1_part3.svg');
+saveas(f3,str)
+
 end
