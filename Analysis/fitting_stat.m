837abc076852459883e8716cb368b9fca741267b
Ben Fulcher <ben.d.fulcher@gmail.com>
2019-11-18 16:56:14 +1100

    Minimal

--- Analysis/fitting_stat.m
+++ Analysis/fitting_stat.m
@@ -1,18 +1,17 @@
-% usage:
-% whatfitMethods is a cell containing one or more methods as strings
 function [adjRSquare,fitObject,fHandle]=fitting_stat(whatfitMethods, xData, yData)
-    % initializa
-    adjRSquare=struct();
-    fitObject=struct();
-    fHandle=struct();
-    for j=1:length(whatfitMethods)
-        [f_handle,Stats,c] = GiveMeFit(xData,yData,whatfitMethods{j},true);
-        fitObject.(whatfitMethods{j})=c;
-        fHandle.(whatfitMethods{j})=f_handle;
-        adjRSquare.(whatfitMethods{j})=Stats.adjrsquare;
-        % confInt.(whatfitMethods{j})=confint(c,0.95);
-        % coeffValue.(whatfitMethods{j})=coeffvalues(c);
-    end
+% USAGE: % whatfitMethods is a cell containing one or more methods as strings
+% Initialize
+adjRSquare=struct();
+fitObject=struct();
+fHandle=struct();
+for j=1:length(whatfitMethods)
+    [f_handle,Stats,c] = GiveMeFit(xData,yData,whatfitMethods{j},true);
+    fitObject.(whatfitMethods{j})=c;
+    fHandle.(whatfitMethods{j})=f_handle;
+    adjRSquare.(whatfitMethods{j})=Stats.adjrsquare;
+    % confInt.(whatfitMethods{j})=confint(c,0.95);
+    % coeffValue.(whatfitMethods{j})=coeffvalues(c);
+end
 %     %%
 %     matAdjRSquare=zeros(length(timePointNow),length(whatfitMethods));
 %     for k=1:length(timePointNow)
--- Analysis/getFitting.m
+++ Analysis/getFitting.m
@@ -1,25 +1,27 @@
-function [fitting_stat_all, constantOut]=getFitting(xData,yData,whatConstantOut)
-  % xData and yData are cells each containing distances and correlation coefficient of all time points
-  % dataProcessing: 'original' or 'binned numThresholds=xx'
-  timePoints=GiveMeParameter('timePoints');
-  % initialize
-  fitting_stat_all = struct();
-  constantOut=zeros(length(timePoints),1);
-  for i = 1:length(timePoints)
-      [fitting_stat_all.(timePoints{i}).adjRSquare, ...
-      fitting_stat_all.(timePoints{i}).fitObject,...
-      fitting_stat_all.(timePoints{i}).fHandle]=fitting_stat({'exp'}, ...
-                                                xData{i}, yData{i}); % only keep exp for the time being
-      % fitting_stat_all.(timePoints{i}).fHandle]=fitting_stat({'linear','exp_1_0','exp1','exp'}, ...
-      %                                           xData{i}, yData{i});
-      % collect the constant
-      switch whatConstantOut
-      case 'decayConstant'
-        constantOut(i)=fitting_stat_all.(timePoints{i}).fitObject.exp.n;
-      case 'offset'
-        constantOut(i)=fitting_stat_all.(timePoints{i}).fitObject.exp.B;
-      case 'strength'
-        constantOut(i)=fitting_stat_all.(timePoints{i}).fitObject.exp.A;
-      end
-  end
+function [fitting_stat_all,constantOut] = getFitting(xData,yData,whatConstantOut)
+% xData and yData are cells each containing distances and correlation coefficient of all time points
+% dataProcessing: 'original' or 'binned numThresholds=xx'
+timePoints = GiveMeParameter('timePoints');
+
+% Initialize:
+fitting_stat_all = struct();
+constantOut = zeros(length(timePoints),1);
+for i = 1:length(timePoints)
+    [fitting_stat_all.(timePoints{i}).adjRSquare, ...
+    fitting_stat_all.(timePoints{i}).fitObject,...
+    fitting_stat_all.(timePoints{i}).fHandle]=fitting_stat({'exp'}, ...
+                                            xData{i}, yData{i}); % only keep exp for the time being
+    % fitting_stat_all.(timePoints{i}).fHandle]=fitting_stat({'linear','exp_1_0','exp1','exp'}, ...
+    %                                           xData{i}, yData{i});
+    % collect the constant
+    switch whatConstantOut
+    case 'decayConstant'
+        constantOut(i) = fitting_stat_all.(timePoints{i}).fitObject.exp.n;
+    case 'offset'
+        constantOut(i) = fitting_stat_all.(timePoints{i}).fitObject.exp.B;
+    case 'strength'
+        constantOut(i) = fitting_stat_all.(timePoints{i}).fitObject.exp.A;
+    end
+end
+
 end
