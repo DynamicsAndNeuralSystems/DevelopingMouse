c0e7aa3eebfc639fc62dd9bb2ba94e925e375cc9
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2020-03-29 21:45:21 +0800

    changed make MaxDistance

--- DataProcessing/getMaxDistance.m
+++ DataProcessing/getMaxDistance.m
@@ -1,10 +1,14 @@
-function [maxDistance] = getMaxDistance(thisBrainDiv,timePointNow)
+function [maxDistance] = getMaxDistance(thisBrainDiv,timePointNow,dimension)
+  % dimension is either 1, 2 or 3 (corresponding to the x, y and z dimensions respecitvely)
+  if nargin < 3
+    dimension = 1;
+  end
   load('annotationGrids.mat');
   timePoints = GiveMeParameter('timePoints');
   timePointIndex = find(strcmp(timePointNow,timePoints));
   resolutionGrid = GiveMeParameter('resolutionGrid');
   % get the max extent along the x-axis
   coOrds = getCoOrds(thisBrainDiv,timePointNow);
-  maxX = max(coOrds(:,1)) - min(coOrds(:,1));
+  maxX = max(coOrds(:,dimension)) - min(coOrds(:,dimension));
   maxDistance = maxX*resolutionGrid.(timePointNow);
 end
--- DataRendering/makeMaxDistance.m
+++ DataRendering/makeMaxDistance.m
@@ -1,8 +1,11 @@
-function [maxDistanceMat] = makeMaxDistance()
+function [maxDistanceMat] = makeMaxDistance(dimension)
+  if nargin < 1
+    dimension = 1;
+  end
   timePoints = GiveMeParameter('timePoints');
   thisBrainDiv = 'wholeBrain';
   maxDistanceMat = zeros(length(timePoints),1);
   for i=1:length(timePoints)
-    maxDistanceMat(i) = getMaxDistance(thisBrainDiv,timePoints{i});
+    maxDistanceMat(i) = getMaxDistance(thisBrainDiv,timePoints{i},dimension);
   end
 end
