1d84a1afa608ed3c04e083bbc033ea0f101d57dd
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2020-07-06 21:31:52 +0800

    Resolved compatibility issue in createDevMouseGeneExpression

--- DataProcessing/createDevMouseGeneExpression.m
+++ DataProcessing/createDevMouseGeneExpression.m
@@ -1,7 +1,7 @@
 function createDevMouseGeneExpression()
   % Convert gene expression data into variable structure fields ``raw, norm` and `normGene`
-  geneEntrez = xlsread('SDK_geneEntrez.csv');
-  [~,geneList,~]  = xlsread('SDK_geneAbbreviations.csv');
+  geneEntrez = readmatrix('SDK_geneEntrez.csv');
+  geneList = readcell('SDK_geneAbbreviations.csv');
   % [structures,Exp,geneEntrez,geneList,timePoints]=LoadData_SDK_full_data_level5('multiple','scaledSigmoid');
   str = fullfile('Matlab_variables','DevMouseGeneExpression.mat');
   save(str,'geneEntrez','geneList')
