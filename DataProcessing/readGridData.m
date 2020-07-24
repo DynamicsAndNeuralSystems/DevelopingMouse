95dcbce12ec508a3717b54d6888b4568b13ae1f6
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2020-07-24 20:40:45 +0800

    Integrated P56 into readGridData

--- DataProcessing/readGridData.m
+++ DataProcessing/readGridData.m
@@ -28,37 +28,13 @@ else
     A = dir(expression_loc);
     % remove hidden files
     A = A(arrayfun(@(A) A.name(1), A) ~= '.');
-
-    if strcmp(timePointNow,'P56') % cater for duplicate experiments for a gene in P56
-      dataInfo = arrayfun(@(A) split(A.name,'_'),A,'UniformOutput',false);
-      % extract the gene IDs
-      gene_ID = zeros(length(dataInfo),1);
-      for i = 1:length(dataInfo)
-        gene_ID(i) = str2double(dataInfo{i}(2));
-      end
-      % indices to unique gene IDs
-      [~,uniqueIx,~] = unique(gene_ID,'stable');
-      Ix = [1:numel(gene_ID)];
-      % duplicate indices
-      [~,dupIx] = setdiff(Ix,uniqueIx,'stable'); %gets index of the first duplicate
-      % find the duplicate grid matrices
-      dupGroups = cell(length(dupIx),1);
-      for i = 1:length(dupIx)
-        dupGroups{i} = find(gene_ID(dupIx(i)) == gene_ID);
-      end
-      [dupGroups,~,~] = uniquecell(dupGroups); % keep unique dupGroups only
-
-      % So now each file is a gene corresponding to this time point
-      numData = length(A);
-    else
-      numData = length(A);
-    end
+    numGenes = length(A);
 end
 
 % Initialize variables
-energyGrids = cell(numData,1);
+energyGrids = cell(numGenes,1);
 % Store IDs of each gene imported
-geneIDInfo = zeros(numData,1);
+geneIDInfo = zeros(numGenes,1);
 
 % Store original directory and move to new directory (necessitated by
 % filepath problems)
@@ -68,7 +44,7 @@ cd(expression_loc);
 %-------------------------------------------------------------------------------
 h = waitbar(0,sprintf('Compiling energy grid for %s...',timePointNow));
 %-------------------------------------------------------------------------------
-for j = 1:numData
+for j = 1:numGenes
     if procParams.usePersistentGenes
         if strcmp(procParams.thisCellType,'allCellTypes')
             fileStr = fullfile(sprintf('%s_%s',fileTimePoints{timePointIndex},...
@@ -96,40 +72,12 @@ for j = 1:numData
         infoStr = strsplit(A(j).name,'_');
         geneIDInfo(j) = str2double(infoStr{2});
     end
-    waitbar(j/numData)
+    waitbar(j/numGenes)
 end
 close(h)
 
 % return back to base directory
 cd(currentFolder);
-if strcmp(timePointNow,'P56')
-  % average duplicate experiments (i.e. experiments for same genes)
-  meanGrid = cell(length(dupGroups),1);
-  for i = 1:length(dupGroups)
-    meanGrid{i} = mean(cat(4,energyGrids{dupGroups{i}}),4);
-  end
-  % read all the grids into a cell
-  energyGridsMean = cell(length(uniqueIx),1);
-  meanGridCounter = 1; % counts mean Grid (duplicate experiments)
-  indexCounter = 1; % counts numData
-  uniqueIxCounter = 1; % counts unique index
-
-  while indexCounter <= length(numData)
-    if indexCounter == uniqueIx(uniqueIxCounter) % unique experiment
-      energyGridsMean{uniqueIxCounter} = energyGrids{indexCounter};
-      uniqueIxCounter = uniqueIxCounter + 1;
-      indexCounter = indexCounter + 1;
-    else
-      energyGridsMean{uniqueIxCounter} = meanGrid{meanGridCounter}; % averaged experiments
-      indexCounter = indexCounter + length(dupGroups{meanGridCounter});
-      meanGridCounter = meanGridCounter + 1;
-    end
-  end
-  energyGrids = energyGridsMean;
-end
-fprintf(1,'indexCounter=%d\n',indexCounter);
-fprintf(1,'uniqueIxCounter=%d\n',uniqueIxCounter);
-fprintf(1,'meanGridCounter=%d\n',meanGridCounter);
 %-------------------------------------------------------------------------------
 %% SAVE
 % Gridded expression energy data:
