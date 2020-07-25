ccfba3d8b8fae1dc96300ddaf0ea2ee2075b96bb
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2020-07-25 08:05:11 +0800

    removed use goodgenesubset from readGridData

--- DataProcessing/readGridData.m
+++ DataProcessing/readGridData.m
@@ -16,20 +16,12 @@ sizeGrids = GiveMeParameter('sizeGrids');
 % Location of saved API gene expression data expressed in a cell
 expression_loc = fullfile('Data','API','GridData',fileTimePoints{timePointIndex});
 
-if procParams.usePersistentGenes
-    load('goodGeneSubset.mat','goodGeneSubset');
-    if ~strcmp(procParams.thisCellType,'allCellTypes')
-        load('enrichedGenes.mat','enrichedGenes');
-        numGenes = length(enrichedGenes.(procParams.thisCellType).ID);
-    else
-        numGenes = length(goodGeneSubset);
-    end
-else
-    A = dir(expression_loc);
-    % remove hidden files
-    A = A(arrayfun(@(A) A.name(1), A) ~= '.');
-    numGenes = length(A);
-end
+
+A = dir(expression_loc);
+% remove hidden files
+A = A(arrayfun(@(A) A.name(1), A) ~= '.');
+numGenes = length(A);
+
 
 % Initialize variables
 energyGrids = cell(numGenes,1);
@@ -45,33 +37,16 @@ cd(expression_loc);
 h = waitbar(0,sprintf('Compiling energy grid for %s...',timePointNow));
 %-------------------------------------------------------------------------------
 for j = 1:numGenes
-    if procParams.usePersistentGenes
-        if strcmp(procParams.thisCellType,'allCellTypes')
-            fileStr = fullfile(sprintf('%s_%s',fileTimePoints{timePointIndex},...
-                        num2str(goodGeneSubset{j})),'energy.raw');
-        else
-            fileStr = fullfile(sprintf('%s_%s',fileTimePoints{timePointIndex},...
-                  num2str(enrichedGenes.(procParams.thisCellType).ID(j))),'energy.raw');
-        end
-    else
-        fileStr = fullfile(A(j).name,'energy.raw');
-    end
+
+    fileStr = fullfile(A(j).name,'energy.raw');
+
     % ENERGY = 3-D matrix of expression energy grid volume
     % load files
     fid = fopen(fileStr, 'r', 'l' );
     energyGrids{j} = fread(fid,prod(sizeGrids.(timePoints{timePointIndex})),'float');
     fclose(fid);
     energyGrids{j} = reshape(energyGrids{j},sizeGrids.(timePoints{timePointIndex}));
-    if procParams.usePersistentGenes
-        if strcmp(procParams.thisCellType,'allCellTypes')
-            geneIDInfo(j) = goodGeneSubset{j};
-        else
-            geneIDInfo(j) = (enrichedGenes.(procParams.thisCellType).ID(j));
-        end
-    else
-        infoStr = strsplit(A(j).name,'_');
-        geneIDInfo(j) = str2double(infoStr{2});
-    end
+
     waitbar(j/numGenes)
 end
 close(h)
@@ -82,11 +57,7 @@ cd(currentFolder);
 %% SAVE
 % Gridded expression energy data:
 cellTypeStr = GiveMeFileName(procParams.thisCellType);
-if (procParams.usePersistentGenes & strcmp(procParams.thisCellType,'allCellTypes'))
-    fileOut = sprintf('energyGrids_goodGeneSubset_%s.mat',timePoints{timePointIndex});
-else
-    fileOut = sprintf('energyGrids%s_%s.mat',cellTypeStr,timePoints{timePointIndex});
-end
+fileOut = sprintf('energyGrids%s_%s.mat',cellTypeStr,timePoints{timePointIndex});
 fileName = fullfile('Matlab_variables',fileOut);
 save(fileName,'energyGrids','geneIDInfo','-v7.3')
 fprintf(1,'Saved processed energy grid information to ''%s''.\n',fileName);
